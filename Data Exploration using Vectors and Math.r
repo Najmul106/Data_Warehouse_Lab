e3f87ea07fcbe9969d0934109fdf0b2edaf0400c
Najmul Hasan <183962560+Najmul106@users.noreply.github.com>
2026-08-25 22:18:01 +0530

    P3

---
+++ Data Exploration using Vectors and Math.r
@@ -0,0 +1,10 @@
+# Creating a vector of numbers
+numbers <- c(13, 21, 35, 42, 58)
+
+# Exploring the data using built-in math functions
+length(numbers)  # Finds how many items are in the vector
+min(numbers)     # Finds the lowest value
+max(numbers)     # Finds the highest value
+mean(numbers)    # Calculates the average
+median(numbers)  # Finds the middle value
+sum(numbers)     # Adds all values together
