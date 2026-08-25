3590c2ee78f08849ceab2bbace5aec82058b9d4e
Najmul Hasan <183962560+Najmul106@users.noreply.github.com>
2026-08-25 22:17:13 +0530

    p1

---
+++ R Variables and Data Types.r
@@ -0,0 +1,13 @@
+# Assigning variables
+name <- "John"
+age <- 40
+is_R_fun <- TRUE
+
+# Outputting variables
+print(name)
+print(age)
+
+# Checking the data type using class()
+class(name)      # Returns "character"
+class(age)       # Returns "numeric"
+class(is_R_fun)  # Returns "logical"
