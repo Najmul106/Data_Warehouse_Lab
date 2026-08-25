0efd4ad74c19c9a5a7b70283c85412fa4c7f5918
Najmul Hasan <183962560+Najmul106@users.noreply.github.com>
2026-08-25 22:18:25 +0530

    P4

---
+++ Arithmetic and Comparison Operators.r
@@ -0,0 +1,19 @@
+x <- 10
+y <- 5
+
+# --- Arithmetic Operations ---
+x + y     # Addition
+x - y     # Subtraction
+x * y     # Multiplication
+x / y     # Division
+x ^ y     # Exponent
+x %% y    # Modulus (Returns the remainder)
+x %/% y   # Integer Division
+
+# --- Relational (Comparison) Operations ---
+x == y    # Equal to
+x != y    # Not equal
+x > y     # Greater than
+x < y     # Less than
+x >= y    # Greater than or equal to
+x <= y    # Less than or equal to
